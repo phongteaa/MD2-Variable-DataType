0e0416ad43f8e04599fb189d0ed88c526863e944
phongteaa <phongteaa@gmail.com>
2022-03-21 09:50:40 +0700

    1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 100;
+        float f = 20.5f;
+        double d = 40.6;
+        boolean b = true;
+        char c = 'C';
+        String str = "Vietnam";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("str = " + str);
+    }
+}
